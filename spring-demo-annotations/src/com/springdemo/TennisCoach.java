ba84a82e1cb10f090cfc51a9d0d6ecca660903b7
Kishor Davara <kdavara@saba.com>
2019-03-17 18:48:12 +0530

    chrore(spring):annotations and autowiring using constructor and setter methods

---
+++ spring-demo-annotations/src/com/springdemo/TennisCoach.java
@@ -0,0 +1,44 @@
+package com.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+//@Component("thatSillyCoach")
+@Component
+public class TennisCoach implements Coach {
+
+	private FortuneService fortuneServie;
+	
+//	@Autowired
+//	public TennisCoach(FortuneService fortuneServie) {
+//		this.fortuneServie = fortuneServie;
+//	}
+	
+    public TennisCoach() {
+    	System.out.println("TennisCoach: Inside default constructor ");
+    }
+	
+//    @Autowired
+//	public void setFortuneServie(FortuneService fortuneServie) {
+//    	System.out.println("TennisCoach: Inside setFortuneServie method");
+//		this.fortuneServie = fortuneServie;
+//	}
+    
+    @Autowired
+	public void doSomeCrazyStuff(FortuneService fortuneServie) {
+    	System.out.println("TennisCoach: Inside doSomeCrazyStuff method");
+		this.fortuneServie = fortuneServie;
+	}
+	
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your backhand volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneServie.getFortune();
+	}
+
+}
