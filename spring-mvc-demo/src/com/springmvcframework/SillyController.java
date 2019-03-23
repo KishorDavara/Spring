527b53c0c1ae97fcd94ddb004d4c0cf40a5b3ff5
Kishor Davara <kishordavara.patel49@gmail.com>
2019-03-23 12:15:30 +0530

    chore(spring-mvc):root level RequestMapping

---
+++ spring-mvc-demo/src/com/springmvcframework/SillyController.java
@@ -0,0 +1,13 @@
+package com.springmvcframework;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SillyController {
+
+	@RequestMapping("/showForm")
+	private String displayForm() {
+		return "silly-form";
+	}
+}
