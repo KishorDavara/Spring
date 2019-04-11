3924533740731cdf8772e41176612d9af1a306b1
Kishor Davara <kishordavara.patel49@gmail.com>
2019-04-11 23:38:13 +0530

    chore(spring-mvc):integer validations

--- spring-mvc-demo/src/com/springmvcframework/Customer.java
+++ spring-mvc-demo/src/com/springmvcframework/Customer.java
@@ -20,12 +20,14 @@ public class Customer {
 	@Size(min = 1, message = "is required")
 	private String lastName;
 	
+	@NotNull(message = "is required")
 	@Min(value = 0, message="must be greater than or equal to zero")
 	@Max(value = 10, message="must be less than or equal to ten")
-	private int freePasses;
+	private Integer freePasses;
 	
 	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
 	private String postalCode;
+	
 
 	public String getFirstName() {
 		return firstName;
@@ -43,11 +45,11 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
 
-	public void setFreePasses(int freePasees) {
+	public void setFreePasses(Integer freePasees) {
 		this.freePasses = freePasees;
 	}
 
